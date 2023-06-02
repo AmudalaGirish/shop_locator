cfca7cba7f333867b558a194f5d5813c3125987b
Girish Amudala <amudalagirish@gmail.com>
2023-06-02 16:32:58 +0530

    ready to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shoplocator.wsgi --log-file -
--- shoplocator/settings.py
+++ shoplocator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iv@fpa!^ju7nb)+=ve=3g00w)hjiwzjxlnz-%uv#!-!p5t2j_z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shoplocator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
